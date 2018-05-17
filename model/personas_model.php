9053a608601ca86a758327357c47a09c3973aa5b
Eliezer <eliezer@Mac-mini.local>
2018-05-17 12:10:26 +0200

    Example project with MVC structure, implemented with PHP

---
+++ model/personas_model.php
@@ -0,0 +1,23 @@
+<?php
+
+class personas_model{
+
+  private $db;
+  private $personas;
+
+  public function __construct(){
+    $this->db = Conectar::conexion();
+    $this->personas = array();
+  }
+
+  public function get_personas(){
+
+    $consulta = $this->db->query("select * from personas;");
+
+    while($filas = $consulta->fetch_assoc()){
+
+      return $this->personas;
+    }
+  }
+}
+ ?>
